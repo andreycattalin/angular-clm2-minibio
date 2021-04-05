76c2b6b268fe01b534d23d55a43e51a6487a0ddb
Andrey <cattalin.htc@gmail.com>
2021-04-05 12:19:07 +0200

    new social network

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,11 @@ export class AppComponent {
       img: faLinkedin,
       text: "Mira mi linkedin",
       link: "https://www.linkedin.com/in/andreymarin/"
+    },
+    {
+      img: faGithub,
+      text: "Mira de nuevo github",
+      link: "https://github.com/andreycattalin/"
     }
   ]
 
